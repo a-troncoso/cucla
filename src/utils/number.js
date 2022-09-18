f2f163cca3250d779db043a4f61fb815557aedfd
Alvaro Troncoso <altroncoso@namutek.com>
2022-09-18 00:49:08 -0300

    update thousandFormat function

--- src/utils/number.js
+++ src/utils/number.js
@@ -1,3 +1,5 @@
+import numeral from 'numeral';
+
 const _regExp = new RegExp(/(\d)(?=(\d{3})+(?!\d))/, 'g');
 
 const _formatFloat = amount =>
@@ -13,27 +15,10 @@ export const extractNumbers = value => {
   return value ? +value.toString().match(/\d+/g).join('') : 0;
 };
 
-export const thousandFormat = (
-  number,
-  decimals,
-  dec_point = ',',
-  thousands_sep = '.',
-) => {
-  let n = !isFinite(+number) ? 0 : +number,
-    prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
-    toFixedFix = function (n, prec) {
-      // Fix for IE parseFloat(0.55).toFixed(0) = 0;
-      let k = Math.pow(10, prec);
-      return Math.round(n * k) / k;
-    },
-    s = (prec ? toFixedFix(n, prec) : Math.round(n)).toString().split('.');
-
-  if (s[0].length > 3) {
-    s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, thousands_sep);
+export const thousandFormat = (number, decimals) => {
+  if (decimals) {
+    const format = `0,0.${'0'.repeat(decimals)}`;
+    return numeral(number).format(format);
   }
-  if ((s[1] || '').length < prec) {
-    s[1] = s[1] || '';
-    s[1] += new Array(prec - s[1].length + 1).join('0');
-  }
-  return s.join(dec_point);
+  return numeral(number).format('0,0');
 };
